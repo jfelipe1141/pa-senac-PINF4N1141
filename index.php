d12955a12ab65b644948822ee5515dca44607300
jfelipe1141 <felip31972@gmail.com>
2015-10-28 21:11:18 -0200

    comit inicial 28/10/15

---
+++ index.php
@@ -0,0 +1,156 @@
+<html>
+    <head>
+        <title>Minha Homepage</title>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+
+
+    </head>
+    <body>
+
+
+        <div class="container">
+            <div class="row">
+
+                <div id="header" title="SITE" class="col-lg-12" >
+                    <div class="jumbotron">
+                        <h1 style="color:red">NOME</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12 alert-success" role="alert">
+                    <ul class="nav nav-pills">
+                        <li><a href="#" title="Home"><h2>Início</h2></a></li>
+                        <li><a href="#" title="Home"><h2>Arquivos</h2></a></li>
+                        <li><a href="#" title="Home"><h2>Usuário</h2></a></li>
+                        <li><a href="#" title="Home"><h2>Contatos</h2></a></li>
+                        <li><a href="#" title="Home"><h2>Cadastro</h2></a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6">
+
+                    <form class="form-horizontal">
+                        <fieldset>
+                            <legend>Formulário</legend>
+                            <div class="form-group">
+                                <label for="inputNome" class="col-lg-2 control-label">Nome</label>
+                                <div class="col-lg-10">
+                                    <input type="text" class="form-control" id="inputNome" placeholder="Nome">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="inputNome" class="col-lg-2 control-label">SOBRE NOME</label>
+                                <div class="col-lg-10">
+                                    <input type="text" class="form-control" id="inputNome" placeholder="Nome">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="inputSexo" class="col-lg-2 control-label">Sexo</label>
+                                <div class="col-lg-10">
+                                    <input type="text" class="form-control" id="inputEmail" placeholder="Sexo">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="inputEmail" class="col-lg-2 control-label">E-mail</label>
+                                <div class="col-lg-10">
+                                    <input type="text" class="form-control" id="inputEmail" placeholder="E-mail">
+                                </div>
+                            </div>
+
+
+
+                            <div class="form-group">
+                                <label for="inputSenha" class="col-lg-2 control-label">Senha</label>
+                                <div class="col-lg-10">
+                                    <input type="text" class="form-control" id="Senha" placeholder="Senha">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPassword" class="col-lg-2 control-label">Confirmar Senha</label>
+                                <div class="col-lg-10">
+                                    <input type="password" class="form-control" id="inputPassword" placeholder="Confirmar Senha">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"> Checagem
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="textArea" class="col-lg-2 control-label">Mensagem</label>
+                                <div class="col-lg-10">
+                                    <textarea class="form-control" rows="3" id="textArea"></textarea>
+                                    <span class="help-block"></span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="select" class="col-lg-2 control-label"></label>
+                                <div class="col-lg-10">
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-lg-10 col-lg-offset-2">
+                                    <button type="reset" class="btn btn-default">Cancel</button>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+
+
+                <div class="col-lg-4">
+
+
+                </div>
+
+            </div>
+
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="alert alert-success" role="alert"></div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row" id="footer">
+                <div class="col-lg-6" >
+                    <img src="4.gif" width="340" height="380" align="middle"/>
+                </div>
+                <div class="col-lg-6" >
+                    <ul class="list-group">
+                        <li class="list-group-item"><h3>Identificação</h3></li>
+                        <li class="list-group-item"><h3>Acessos</h3></li>
+                        <li class="list-group-item"><h3>Contatos</h3></li>
+                        <li class="list-group-item"><h3>Busca</h3></li>
+                        <li class="list-group-item"><h3>Ajuda</h3></li>
+                    </ul>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="alert alert-success" role="alert"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
