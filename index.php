518c3aaa2376e6333adcef089d32adc446b823f5
jfelipe1141 <felip31972@gmail.com>
2015-11-25 19:41:25 -0200

    apresentacao final

--- index.php
+++ index.php
@@ -27,10 +27,10 @@
                 </div>
                 <div class="col-sm-4 col-lg-4 ">
                     <div class="thumbnail">
-                        <img src="img/1.jpg" alt="">
+                        <img src="img/-Knight-Rider.jpg" alt="">
                         <div class="caption">
-                            <h4 class="pull-right">R$24.99</h4>
-                            <h4><a href="#">Furgao Esquadrao classe A</a>
+                            <h4 class="pull-right">R$30.00</h4>
+                            <h4><a href="#">SuperMaquina Pontiac Trans Am 1982</a>
                             </h4>
                             <p>Escala 1:18</p>
                         </div>
@@ -42,10 +42,10 @@
                 </div>
                 <div class="col-sm-4 col-lg-4 ">
                     <div class="thumbnail">
-                        <img src="img/1.jpg" alt="">
+                        <img src="img/charger.toretto.JPG" alt="">
                         <div class="caption">
-                            <h4 class="pull-right">R$24.99</h4>
-                            <h4><a href="#">Furgao Esquadrao classe A</a>
+                            <h4 class="pull-right">R$40.00</h4>
+                            <h4><a href="#">Dodge Charger R/T 1970 Fast and Furious</a>
                             </h4>
                             <p>Escala 1:18</p>
                         </div>
